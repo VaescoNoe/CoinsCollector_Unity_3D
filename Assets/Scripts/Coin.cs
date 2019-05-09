0037dc26a68bf36c265cd80a196e89396174d737
Noe <VaescoNoe@gmail.com>
2019-05-09 00:23:47 -0500

    Se agrega temporalizador

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public static int coinsCount = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        Coin.coinsCount++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     void OnDestroy()
+    {
+        Coin.coinsCount--;
+        if (Coin.coinsCount <= 0)
+        {
+            Debug.Log("El juego ha terminado");
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    } 
+} 
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float maxTime = 60.0f;
+
+    private float countDown = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        countDown = maxTime;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            Coin.coinsCount = 0;
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
